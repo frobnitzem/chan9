c8ab7d7c8c9719c677833aaad400089e31002178
David M. Rogers <predictivestatmech@gmail.com>
2012-09-23 22:43:38 -0600

    Fixed create directory from cl.go

--- p/clnt/examples/cl/cl.go
+++ p/clnt/examples/cl/cl.go
@@ -240,7 +240,7 @@ Outer:
 // Create a single directory on remote server
 func mkone(c *clnt.Clnt, s string) {
 	fname := normpath(s)
-	file, oserr := c.FCreate(fname, 0777|p.DMDIR, p.OWRITE)
+	file, oserr := c.FCreate(fname, 0777|p.DMDIR, p.OREAD)
 	if oserr != nil {
 		fmt.Fprintf(os.Stderr, "error creating directory %s: %v\n", fname, oserr)
 		return
